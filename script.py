33b6506c6bf9fa719cc2d709d6461e1d8164bd5b
Kritsanai Hoisang <ksnn@Kritsanais-MacBook-Pro.local>
2024-11-07 00:52:21 +0700

    use oop

--- script.py
+++ script.py
@@ -2,32 +2,30 @@ import cv2
 import os
 import makeDirec as mdir
 
-# videoName = "chiangrak1_back"
-# directoryName = videoName
-# frameStep = 40
+videoName = "chiangrak1_back"
+directoryName = videoName
+frameStep = 40
 
-# video = cv2.VideoCapture(f"sources/{videoName}.mp4")
+video = cv2.VideoCapture(f"sources/{videoName}.mp4")
 
-# frameCount = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+frameCount = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# print(f'have farme: {frameCount}')
+print(f'have farme: {frameCount}')
 
-# for i in range(frameCount):
-#     check, frame = video.read()
+for i in range(frameCount):
+    check, frame = video.read()
     
-#     if i % frameStep == 0:
-#         cv2.imshow("cap", frame)
+    if i % frameStep == 0:
+        cv2.imshow("cap", frame)
         
-#         mdir.makeDirectory(directoryName) # make directory if not have ever 
-#         cv2.imwrite(f"output/{directoryName}/{videoName}_img{i}.jpg", frame)
+        mdir.makeDirectory(directoryName) # make directory if not have ever 
+        cv2.imwrite(f"output/{directoryName}/{videoName}_img{i}.jpg", frame)
         
-#         if cv2.waitKey(1) & 0xFF == ord('e'):
-#             break
+        if cv2.waitKey(1) & 0xFF == ord('e'):
+            break
 
-# video.release()
-# cv2.destroyAllWindows()
+video.release()
+cv2.destroyAllWindows()
 
-
-mdir.makeDirectory("test") # make directory if not have ever 
 
 
